478ba8f32360f750d9cb4b1208918ef6c3a09e7d
Marcelo Canaparro <Marcelo.Canaparro@ctw.bmwgroup.com>
2022-12-11 17:29:05 +0000

    fixes vpc id again

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ module "blog_security_group" {
   version     = "4.16.2"
   name        = "blog_new_module"
 
-  vpc_id              = module.vpc.vpc_id
+  vpc_id              = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
